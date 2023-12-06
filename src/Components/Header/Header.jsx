2a4ad03b1aa8b0e4b19af7d5500fc878ef491b83
Jahongir0126 <tursunboyevjahongir04@mail.ru>
2023-12-07 00:24:28 +0500

    header full but not localize

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 
 import logo from "/img/logo.png";
+import drone1 from "/img/navbar_dron_photo_1.png";
+import drone2 from "/img/navbar_dron_photo_2.png";
+import mobile_phone from "/img/navbar_mobile_phone.png";
 
 export default function Header() {
   return (
@@ -27,67 +30,91 @@ export default function Header() {
             <div className="collapse navbar-collapse " id="navbarNavDropdown">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 hamburger_menu">
                 <li className="nav-item fw-medium ms-3 my_dropdown">
-                  <Link
+                  <Link to={""}
                     className="nav-link link-dark link-opacity-75-hover"
                   >Products
                   </Link>
                   <div>
                     <ul className="row">
-                      <li className="dropdown-item col">
-                        <h5>Crop Protection</h5>
-                        <h6>Efficient & Autonomous Operation</h6>
-                        <p>AGRAS T20P</p>
-                        <p>AGRAS T40</p>
-                        <p>AGRAS T30</p>
-                        <p>AGRAS T10</p>
+                      <li className="dropdown-item  d-flex justify-content-between">
+                        <img src={drone1} alt="drone photo" className="navbar_img" />
+                        <div>
+                          <h5>Crop Protection</h5>
+                          <span className="">Efficient & Autonomous Operation</span>
+                          <Link to={"https://www.dji.com/t20p?site=ag&from=nav"} target="_blank" className="nav-link navbar_p mt-3">AGRAS T20P</Link>
+                          <Link to={"https://www.dji.com/t40?site=ag&from=nav"} target="_blank" className="nav-link navbar_p">AGRAS T40</Link>
+                          <Link to={"https://www.dji.com/t30?site=ag&from=nav"} target="_blank" className="nav-link navbar_p">AGRAS T30</Link>
+                          <Link to={"https://www.dji.com/t10?site=ag&from=nav"} target="_blank" className="nav-link navbar_p">AGRAS T10</Link>
+                        </div>
                       </li>
-                      <li className="dropdown-item col">
-                        <h5>Mission Planning</h5>
-                        <h6>Detect Every Detail</h6>
+                      <li className="dropdown-item  d-flex">
+                        <img src={drone2} alt="drone photo" className="navbar_img" />
+                        <div>
+                          <h5>Mission Planning</h5>
+                          <span>Detect Every Detail</span>
+                          <Link to={"https://ag.dji.com/mavic-3-m?site=ag&from=nav"} target="_blank" className="nav-link navbar_p mt-3">Mavic 3M</Link>
+                          <Link to={"https://www.dji.com/dji-terra?site=ag&from=nav"} target="_blank" className="nav-link navbar_p">DJI Terra</Link>
+                        </div>
+                      </li>
+                      <li className="dropdown-item  d-flex">
+                        <img src={mobile_phone} alt="drone photo" className="navbar_img" />
 
-                        <p>Mavic 3M</p>
-                        <p>DJI Terra</p>
-                      </li>
-                      <li className="dropdown-item col">
-                        <h5>Smart Farm</h5>
-                        <h6>Providing efficient crop protection services</h6>
-                        <p>DJI SmartFarm Web</p>
-                        <p>DJI SmartFarm App</p>
+                        <div>
+                          <h5>Smart Farm</h5>
+                          <span>Providing efficient crop protection services</span>
+                          <Link to={"https://ag.dji.com/smartfarm-web?site=ag&from=nav"} target="_blank" className="nav-link navbar_p mt-3">DJI SmartFarm Web</Link>
+                          <Link to={"https://www.dji.com/ag-platform?site=ag&from=nav"} target="_blank" className="nav-link navbar_p">DJI SmartFarm App</Link>
+                        </div>
                       </li>
                     </ul>
                   </div>
                 </li>
+
                 <li className="nav-item fw-medium ms-3">
-                  <Link
-                    to="/newsroom"
+                  <Link to={"https://ag.dji.com/newsroom?site=ag&from=nav"} target="_blank"
                     className="nav-link link-dark link-opacity-75-hover"
                   >Newsroom
                   </Link>
                 </li>
                 <li className="nav-item fw-medium ms-3">
-                  <Link
-                    to="/insights"
+                  <Link to={"https://ag.dji.com/case-studies?site=ag&from=nav"} target="_blank"
                     className="nav-link link-dark link-opacity-75-hover"
                   >Insights
                   </Link>
                 </li>
                 <li className="nav-item fw-medium ms-3">
-                  <Link
-                    to="/academy"
+                  <Link to={"https://www.uastc.com/us/?site=ag&from=nav"} target="_blank"
                     className="nav-link link-dark link-opacity-75-hover"
                   >DJI Academy
                   </Link>
                 </li>
-                <li className="nav-item fw-medium ms-3">
-                  <Link
-                    to="/support"
-                    className="nav-link link-dark link-opacity-75-hover"
+                <li className="nav-item fw-medium ms-3 dropdown support_dropdown">
+                  <Link className="nav-link link-dark link-opacity-75-hover"
                   >Support
                   </Link>
+                  <ul className="dropdown-menu p-0">
+                    <li>
+                      <Link to={"https://www.dji.com/support?site=ag&from=nav"} target="_blank" class="nav-link dropdown-item">After-Sales Service Policies</Link>
+                    </li>
+                    <li>
+                      <Link to={"https://www.dji.com/where-to-buy/agriculture-dealers?site=ag&from=nav"} target="_blank" class="nav-link dropdown-item">Find Dealers Nearby</Link>
+                    </li>
+                    <li>
+                      <Link to={"https://www.dji.com/flysafe?site=ag&from=nav"} target="_blank" class="nav-link dropdown-item">Fly Safe</Link>
+                    </li>
+                    <li>
+                      <Link to={"https://www.dji.com/downloads?site=ag&from=nav"} target="_blank" class="nav-link dropdown-item">Download Center </Link>
+                    </li>
+                    <li>
+                      <Link to={"https://www.dji.com/service/repair/centers?site=ag&from=nav"} target="_blank" class="nav-link dropdown-item">Service Center </Link>
+                    </li>
+                  </ul>
+
+
+
                 </li>
                 <li className="nav-item fw-medium ms-3">
-                  <Link
-                    to="/about-us"
+                  <Link to={"https://ag.dji.com/about-us?site=ag&from=nav"} target="_blank"
                     className="nav-link link-dark link-opacity-75-hover"
                   >About Us
                   </Link>
